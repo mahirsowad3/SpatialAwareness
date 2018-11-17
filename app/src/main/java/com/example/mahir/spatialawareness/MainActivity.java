27e9ac8282b59a9e8166169b6625a0df289831b7
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-11-16 21:23:13 -0500

    Using TextureView to integrate camera trial session

--- app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
+++ app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.mahir.spatialawareness;
 
+
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textureView = findViewById(R.id.textureView);
+
+        assert textureView != null;
+
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
     }
 }
