9617e6acf54df7058fd85df97a2826b46f8a4c8f
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-11-22 09:47:14 -0500

    Attempt at Frame by Frame Processing like android example

---
+++ app/src/main/java/com/example/mahir/spatialawareness/CameraActivity.java
@@ -0,0 +1,11 @@
+package com.example.mahir.spatialawareness;
+
+import android.app.Activity;
+import android.media.ImageReader;
+
+import java.util.logging.Logger;
+
+public abstract class CameraActivity extends Activity {
+     //private static final Logger LOGGER = new Logger();
+
+}
--- app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
+++ app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.mahir.spatialawareness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.tensorflow.lite.Interpreter;
 
 public class MainActivity extends AppCompatActivity {
 
