9e509d7b4b7e6ce154fa5dcf0a34df55f672c31a
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-26 09:01:32 -0500

    Surfacetexture

--- app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
+++ app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.mahir.spatialawareness;
 
+import android.hardware.Camera;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -13,13 +16,39 @@ import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.view.TextureView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextureView.SurfaceTextureListener {
+
+    private Camera mCamera;
+    private TextureView mTextureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+
+        
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
