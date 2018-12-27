8cea290c28f31d4cca46f95edd096248c57d5165
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-27 11:59:46 -0500

    SetupCamera function--creates CameraManager object and retrieves the proper camera id for backfacing camera Manifest change to lock orientation in portrait

--- app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
+++ app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.mahir.spatialawareness;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextureView cameraTextureView;
+    CameraDevice cameraDevice;
+    private String mCameraId;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraTextureView = findViewById(R.id.CameraField);
+
+    }
+
+    private void takePicture(){
+
+    }
+
+    private void setUpCamera(int width, int height){
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            for(String cameraID: cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraID);
+                if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_FRONT){
+                    continue;
+                }else{
+                    mCameraId = cameraID;
+                    return;
+                }
+            }
+        }catch(CameraAccessException e){
+            e.printStackTrace();
+        }
     }
 }
