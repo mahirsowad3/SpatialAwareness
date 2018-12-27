8fd989ea618916c4f09923bccc22f31833983bb8
mahirsowad3 <32722691+mahirsowad3@users.noreply.github.com>
2018-12-27 13:11:53 -0500

    Setting preview sizes width and height using TextureView

--- app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
+++ app/src/main/java/com/example/mahir/spatialawareness/MainActivity.java
@@ -1,36 +1,78 @@
 package com.example.mahir.spatialawareness;
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.SparseIntArray;
-import android.view.Surface;
+import android.util.Size;
 import android.view.TextureView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextureView cameraTextureView;
+    TextureView mTextureView;
     CameraDevice cameraDevice;
     private String mCameraId;
+    private Size mPreviewSize;
 
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            setUpCamera(width, height);
+        }
 
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        if(mTextureView.isAvailable()){
+
+        }else{
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cameraTextureView = findViewById(R.id.CameraField);
+        mTextureView = findViewById(R.id.CameraField);
 
     }
 
     private void takePicture(){
 
     }
+
+
+
+
 
     private void setUpCamera(int width, int height){
         CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
@@ -42,11 +84,37 @@ public class MainActivity extends AppCompatActivity {
                     continue;
                 }else{
                     mCameraId = cameraID;
+                    StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    mPreviewSize = getPreferredPreviewSize(map.getOutputSizes(SurfaceTexture.class), width, height);
                     return;
                 }
             }
         }catch(CameraAccessException e){
             e.printStackTrace();
         }
+    }
+
+    private Size getPreferredPreviewSize(Size[] mapSizes, int width, int height){
+        List<Size> collectorSizes = new ArrayList<>();
+        for(Size option: mapSizes){
+            if(width > height){
+                if(option.getWidth() > width && option.getHeight() > height){
+                    collectorSizes.add(option);
+                }
+            }else{
+                if(option.getWidth() > height && option.getHeight() > width){
+                    collectorSizes.add(option);
+                }
+            }
+        }
+        if(collectorSizes.size() > 0){
+            return Collections.min(collectorSizes, new Comparator<Size>() {
+                @Override
+                public int compare(Size o1, Size o2) {
+                    return Long.signum(o1.getWidth() * o1.getHeight() - o2.getWidth() * o2.getHeight());
+                }
+            });
+        }
+        return mapSizes[0];
     }
 }
